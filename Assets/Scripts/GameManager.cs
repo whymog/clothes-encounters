d72d07d022c94b232a91707cd1527da871dd0b14
Nick Cummings <nickmcummings@gmail.com>
2020-02-01 14:28:07 -0800

    Rig up input to advance from screen to screen, if screen is Title or Tutorial.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+  // Start is called before the first frame update
+
+  public static string sceneName = "";
+  public static int sceneIndex = 0;
+
+  void Start()
+  {
+    DontDestroyOnLoad(this.gameObject);
+
+    this.UpdateSceneNameAndIndex();
+  }
+
+  void UpdateSceneNameAndIndex()
+  {
+    Scene scene = SceneManager.GetActiveScene();
+    sceneName = scene.name;
+    sceneIndex = scene.buildIndex;
+  }
+
+  // called first
+  void OnEnable()
+  {
+    SceneManager.sceneLoaded += OnSceneLoaded;
+  }
+
+  void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+  {
+    UpdateSceneNameAndIndex();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}
