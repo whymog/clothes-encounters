879ad0fc10f30387314a14e29d6f0b07b1630258
Nick Cummings <nickmcummings@gmail.com>
2020-02-02 11:40:29 -0800

    Rig up score display on results screen.

---
+++ Assets/Scripts/GetAndDisplayScore.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GetAndDisplayScore : MonoBehaviour
+{
+  public int playerNumber;
+  private int score;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    if (playerNumber < 1) playerNumber = 1;
+
+    score = playerNumber == 1 ? GameManager.player1Score : GameManager.player2Score;
+
+    GetComponent<TMPro.TextMeshProUGUI>().text = score.ToString();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}
