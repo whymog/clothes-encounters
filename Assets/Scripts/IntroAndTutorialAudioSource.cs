ca2f4d9ea0d84e52d55a741b7bdf8a0dc5eddc53
Nick Cummings <nickmcummings@gmail.com>
2020-02-02 11:59:19 -0800

    Rig up a script to handle audio continuing to play from the intro into the tutorial screen.

---
+++ Assets/Scripts/IntroAndTutorialAudioSource.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroAndTutorialAudioSource : MonoBehaviour
+{
+  // Start is called before the first frame update
+  void Start()
+  {
+    DontDestroyOnLoad(gameObject);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (GameManager.sceneIndex > 2)
+    {
+      Destroy(gameObject);
+    }
+  }
+}
