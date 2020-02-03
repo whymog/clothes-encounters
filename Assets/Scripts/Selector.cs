ea4567595400b981484f9ccac5e7ce5428afd2ce
Nick Cummings <nickmcummings@gmail.com>
2020-02-02 17:55:35 -0800

    Add SFX to socks. SockFX. Tired.

--- Assets/Scripts/Selector.cs
+++ Assets/Scripts/Selector.cs
@@ -6,6 +6,18 @@ public class Selector : MonoBehaviour
 {
   public static string selectedSock = "";
 
+  public AudioSource audioSource;
+
+  public AudioClip sockBeerAudio;
+  public AudioClip sockCuteAudio;
+  public AudioClip sockFuzzyAudio;
+  public AudioClip sockGooglyAudio;
+  public AudioClip sockGooseAudio;
+  public AudioClip sockPipeCleanerAudio;
+  public AudioClip sockPompomAudio;
+  public AudioClip sockSportAudio;
+  public AudioClip sockStripedAudio;
+
   // Start is called before the first frame update
   void Start()
   {
@@ -18,5 +30,47 @@ public class Selector : MonoBehaviour
     Debug.Log(collider.name);
 
     selectedSock = collider.name;
+
+    AudioClip clipToPlay = sockBeerAudio;
+
+    if (collider.name == "SockBeer")
+    {
+      clipToPlay = sockBeerAudio;
+    }
+    else if (collider.name == "SockCute")
+    {
+      clipToPlay = sockCuteAudio;
+    }
+    else if (collider.name == "SockFuzzy")
+    {
+      clipToPlay = sockFuzzyAudio;
+    }
+    else if (collider.name == "SockGoogly")
+    {
+      clipToPlay = sockGooglyAudio;
+    }
+    else if (collider.name == "SockGoose")
+    {
+      clipToPlay = sockGooseAudio;
+    }
+    else if (collider.name == "SockPipeCleaner")
+    {
+      clipToPlay = sockPipeCleanerAudio;
+    }
+    else if (collider.name == "SockPompom")
+    {
+      clipToPlay = sockPompomAudio;
+    }
+    else if (collider.name == "SockSport")
+    {
+      clipToPlay = sockSportAudio;
+    }
+    else if (collider.name == "SockStriped")
+    {
+      clipToPlay = sockStripedAudio;
+    }
+
+    audioSource.clip = clipToPlay;
+    audioSource.Play(0);
   }
 }
