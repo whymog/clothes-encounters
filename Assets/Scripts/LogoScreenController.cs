b792aa18d558c974f901cd19c2dba544311bd51b
Ben Morgan <benjaminmorgan89@gmail.com>
2020-02-16 15:24:13 -0800

    Added PIGSquad bumper and TGIH logo
    New scenes, assets, and scripts for the PIGSquad bumper video and the TGIH logo screen.

---
+++ Assets/Scripts/LogoScreenController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LogoScreenController : MonoBehaviour
+{
+
+    void Start()
+    {
+        StartCoroutine(SwitchScenes());
+    }
+
+    private IEnumerator SwitchScenes()
+    {
+        yield return new WaitForSeconds(5f);
+
+        SceneManager.LoadScene("Title");
+    }
+}
+
---
+++ Assets/Scripts/PIGSquadVideoController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class PIGSquadVideoController : MonoBehaviour
+{
+    private VideoPlayer videoPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer = gameObject.GetComponent<VideoPlayer>();
+        videoPlayer.Play();
+        StartCoroutine(SwitchScenes());
+    }
+
+    private IEnumerator SwitchScenes()
+    {
+        yield return new WaitForSeconds(5f);
+
+        SceneManager.LoadScene("TGIHLogo");
+    }
+}
