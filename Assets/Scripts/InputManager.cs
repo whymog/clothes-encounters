ab68a7a6b607dd3780b4740f4fc338983837df67
Ben Morgan <benjaminmorgan89@gmail.com>
2020-02-18 11:50:51 -0800

    Added controller support for DS4
    D-pad controls don't work over bluetooth on Mac

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -3,45 +3,45 @@ using UnityEngine.SceneManagement;
 
 public class InputManager : MonoBehaviour
 {
-  // Start is called before the first frame update
-  void Start()
-  {
-    Debug.Log("InputManager is awake.");
-  }
-
-  // Update is called once per frame
-  void Update()
-  {
-    if (Input.GetKeyDown(KeyCode.Space))
+    // Start is called before the first frame update
+    void Start()
     {
-      Debug.Log("Pressed Space");
-      if (GameManager.canAdvanceScene == true && (GameManager.sceneName != "Game" || GameManager.sceneName == "Game" && GameManager.isGameOver == true))
-      {
-        if (GameManager.sceneName == "Results")
-        {
-          SceneManager.LoadScene(2);
-        }
-        else
-        {
-          SceneManager.LoadScene(GameManager.sceneIndex + 1);
-        }
-      }
-      else if (GameManager.sceneName == "Game")
-      {
-        // Handle game input here; hacky, I know, but whatever
-        if (!GameManager.isBetweenTurns && !GameManager.isEndOfTurn)
-        {
-          // Turn is in progress - space makes a selection
-          // GameManager.isEndOfTurn = true;
-        }
-        else if (GameManager.isEndOfTurn)
-        {
-          // Start new turn
-          GameManager.turnNumber++;
-          GameManager.isBetweenTurns = true;
-          GameManager.isEndOfTurn = false;
-        }
-      }
+        Debug.Log("InputManager is awake.");
     }
-  }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Fire2"))
+        {
+            Debug.Log("Pressed Space");
+            if (GameManager.canAdvanceScene == true && (GameManager.sceneName != "Game" || GameManager.sceneName == "Game" && GameManager.isGameOver == true))
+            {
+                if (GameManager.sceneName == "Results")
+                {
+                    SceneManager.LoadScene(2);
+                }
+                else
+                {
+                    SceneManager.LoadScene(GameManager.sceneIndex + 1);
+                }
+            }
+            else if (GameManager.sceneName == "Game")
+            {
+                // Handle game input here; hacky, I know, but whatever
+                if (!GameManager.isBetweenTurns && !GameManager.isEndOfTurn)
+                {
+                    // Turn is in progress - space makes a selection
+                    // GameManager.isEndOfTurn = true;
+                }
+                else if (GameManager.isEndOfTurn)
+                {
+                    // Start new turn
+                    GameManager.turnNumber++;
+                    GameManager.isBetweenTurns = true;
+                    GameManager.isEndOfTurn = false;
+                }
+            }
+        }
+    }
 }
--- Assets/Scripts/Wheel.cs
+++ Assets/Scripts/Wheel.cs
@@ -4,100 +4,100 @@ using UnityEngine;
 
 public class Wheel : MonoBehaviour
 {
-  private Quaternion newRotation;
-  private string rotateDirection;
-  private Component[] socks;
+    private Quaternion newRotation;
+    private string rotateDirection;
+    private Component[] socks;
 
-  // Start is called before the first frame update
-  void Start()
-  {
-    socks = gameObject.GetComponentsInChildren<Transform>();
-
-    float radius = 3f;
-
-    for (int i = 0; i <= 13; i++)
+    // Start is called before the first frame update
+    void Start()
     {
-      float angle = i * Mathf.PI * 2f / 13;
-      socks[i].transform.localPosition = new Vector3(Mathf.Sin(angle) * radius, Mathf.Cos(angle) * radius, 0f);
-      socks[i].transform.localRotation = Quaternion.Euler(0, 0, -(float)i * 360f / 13f);
-    }
+        socks = gameObject.GetComponentsInChildren<Transform>();
 
-    gameObject.transform.localPosition = new Vector3(0, 0, 0);
+        float radius = 3f;
 
-    newRotation = transform.localRotation;
-    rotateDirection = "";
-  }
-
-  // Update is called once per frame
-  void Update()
-  {
-    if (rotateDirection == "")
-    {
-      if (Input.GetKeyDown(KeyCode.LeftArrow))
-      {
-        // rotate left
-        newRotation = transform.localRotation;
-        newRotation *= Quaternion.Euler(0, 0, 360f / 13f);
-        rotateDirection = "clockwise";
-        StartCoroutine(RotateWheel(30));
-      }
-      else if (Input.GetKeyDown(KeyCode.RightArrow))
-      {
-        //rotate right
-        newRotation = transform.localRotation;
-        newRotation *= Quaternion.Euler(0, 0, -360f / 13f);
-        rotateDirection = "counterclockwise";
-        StartCoroutine(RotateWheel(30));
-      }
-      else if (Input.GetKeyDown(KeyCode.Space))
-      {
-        Debug.Log("space!");
-
-        string sockName = GameManager.sockOrder[GameManager.turnNumber - 1];
-
-        if (Selector.selectedSock == sockName)
+        for (int i = 0; i <= 13; i++)
         {
-          GameManager.lastGuessWasCorrect = true;
-          GameManager.player1Score++;
-          GameManager.player2Score++;
-        }
-        else
-        {
-          GameManager.lastGuessWasCorrect = false;
-          if (GameManager.turnNumber % 2 == 0)
-          {
-            GameManager.player2Score--;
-          }
-          else
-          {
-            GameManager.player1Score--;
-          }
+            float angle = i * Mathf.PI * 2f / 13;
+            socks[i].transform.localPosition = new Vector3(Mathf.Sin(angle) * radius, Mathf.Cos(angle) * radius, 0f);
+            socks[i].transform.localRotation = Quaternion.Euler(0, 0, -(float)i * 360f / 13f);
         }
 
-        GameManager.isEndOfTurn = true;
-      }
+        gameObject.transform.localPosition = new Vector3(0, 0, 0);
+
+        newRotation = transform.localRotation;
+        rotateDirection = "";
     }
-  }
 
-  IEnumerator RotateWheel(int frames)
-  {
-    int currentFrame = 0;
-
-    while (currentFrame < frames)
+    // Update is called once per frame
+    void Update()
     {
-      if (rotateDirection == "clockwise")
-      {
-        transform.localRotation *= Quaternion.Euler(0, 0, 360f / (13f * (float)frames));
-      }
-      else if (rotateDirection == "counterclockwise")
-      {
-        transform.localRotation *= Quaternion.Euler(0, 0, -360f / (13f * (float)frames));
-      }
+        if (rotateDirection == "")
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetAxis("Horizontal") > 0 || Input.GetAxisRaw("Horizontal dpad") > 0)
+            {
+                // rotate left
+                newRotation = transform.localRotation;
+                newRotation *= Quaternion.Euler(0, 0, 360f / 13f);
+                rotateDirection = "clockwise";
+                StartCoroutine(RotateWheel(30));
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetAxis("Horizontal") < 0 || Input.GetAxisRaw("Horizontal dpad") < 0)
+            {
+                //rotate right
+                newRotation = transform.localRotation;
+                newRotation *= Quaternion.Euler(0, 0, -360f / 13f);
+                rotateDirection = "counterclockwise";
+                StartCoroutine(RotateWheel(30));
+            }
+            else if (Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Fire2"))
+            {
+                Debug.Log("space!");
 
-      currentFrame++;
-      yield return new WaitForEndOfFrame();
+                string sockName = GameManager.sockOrder[GameManager.turnNumber - 1];
+
+                if (Selector.selectedSock == sockName)
+                {
+                    GameManager.lastGuessWasCorrect = true;
+                    GameManager.player1Score++;
+                    GameManager.player2Score++;
+                }
+                else
+                {
+                    GameManager.lastGuessWasCorrect = false;
+                    if (GameManager.turnNumber % 2 == 0)
+                    {
+                        GameManager.player2Score--;
+                    }
+                    else
+                    {
+                        GameManager.player1Score--;
+                    }
+                }
+
+                GameManager.isEndOfTurn = true;
+            }
+        }
     }
 
-    rotateDirection = "";
-  }
+    IEnumerator RotateWheel(int frames)
+    {
+        int currentFrame = 0;
+
+        while (currentFrame < frames)
+        {
+            if (rotateDirection == "clockwise")
+            {
+                transform.localRotation *= Quaternion.Euler(0, 0, 360f / (13f * (float)frames));
+            }
+            else if (rotateDirection == "counterclockwise")
+            {
+                transform.localRotation *= Quaternion.Euler(0, 0, -360f / (13f * (float)frames));
+            }
+
+            currentFrame++;
+            yield return new WaitForEndOfFrame();
+        }
+
+        rotateDirection = "";
+    }
 }
